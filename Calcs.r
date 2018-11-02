ecf7271e0052be3a34c5bfdce30047fc2ba35101
Rakkhi Samarasekera <rakkhi.s@gmail.com>
2018-11-03 00:11:52 +1100

    Adding data load and basic mean calc

---
+++ Calcs.r
@@ -0,0 +1,7 @@
+# Mean of responses by school
+
+aggregate(Q12018RespData$answer_num, by = list(Q12018RespData$school_name), mean)
+
+# Mean of responses by school by question grouping (standard)
+
+aggregate(Q12018RespData$answer_num, by = list(Q12018RespData$school_name, Q12018RespData$question_group_id), mean)
---
+++ Dataload.r
@@ -0,0 +1,20 @@
+# Read in the data files
+Q12018RespData <- read.csv("C:/users/rakkhi/documents/pivotpl_data/all_responses_2018-01-01_2018-04-12.csv")
+
+# Remove the meta columns:
+Q12018RespData <- Q12018RespData[ -c (17:40)]
+
+# Create a new column to store the answer data as numeric
+Q12018RespData$answer_num<-NA
+
+# Convert responses to numeric
+Q12018RespData$answer_num <-
+  ifelse(Q12018RespData$answer == 'Strongly disagree', 1,
+    ifelse(Q12018RespData$answer == 'Disagree', 2,
+      ifelse(Q12018RespData$answer == 'Neither agree nor disagree', 3,
+        ifelse(Q12018RespData$answer == 'Agree', 4,
+          ifelse(Q12018RespData$answer == 'Strongly agree', 5, 0)
+        )
+      )
+    )
+  )
