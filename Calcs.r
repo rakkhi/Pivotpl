6eaf0749da8a174f10c7dcd0ba60e23ab68d6506
Rakkhi Samarasekera <rakkhi.s@gmail.com>
2018-11-06 22:48:31 +1100

    Adding 2017 data and a number of calcs

--- Calcs.r
+++ Calcs.r
@@ -1,3 +1,103 @@
+#2017 data
+
+# Mean of responses by school by Department
+###################
+
+S12017MeanSchoolCluster <- aggregate(S12017RespData$answer_num, by = list(S12017RespData$acara_id, S12017RespData$cluster_tag), mean)
+
+## Set column names
+colnames(S12017MeanSchoolCluster) <- c("acara_id", "cluster_tag", "answer_num_cluster_mean_school")
+
+## Write this to a file
+write.csv(S12017MeanSchoolCluster, file = "C:/users/rakkhi/documents/pivotpl_data/S12017MeanSchoolCluster.csv")
+
+# Merge the mean per dept per school into the main data set
+S12017RespData <- merge(x = S12017RespData, y = S12017MeanSchoolCluster, by = c("acara_id", "cluster_tag"), all.x = TRUE)
+
+#####################
+
+
+# Mean of responses Department
+###################
+
+S12017MeanCluster <- aggregate(S12017RespData$answer_num, by = list(S12017RespData$cluster_tag), mean)
+
+## Set column names
+colnames(S12017MeanCluster) <- c("cluster_tag", "answer_num_cluster_mean")
+
+## Write this to a file
+write.csv(S12017MeanCluster, file = "C:/users/rakkhi/documents/pivotpl_data/S12017MeanCluster.csv")
+
+# Merge the mean per dept into the main data set
+S12017RespData <- merge(x = S12017RespData, y = S12017MeanSchoolCluster, by = c("cluster_tag"), all.x = TRUE)
+
+#####################
+
+# Mean of responses Department by Education Standard
+###################
+
+S12017MeanClusterEdustd <- aggregate(S12017RespData$answer_num, by = list(S12017RespData$cluster_tag, S12017RespData$question_group_id), mean)
+
+## Set column names
+colnames(S12017MeanClusterEdustd) <- c("cluster_tag", "question_group_id", "answer_num_cluster_mean_edustd")
+
+## Write this to a file
+write.csv(S12017MeanClusterEdustd, file = "C:/users/rakkhi/documents/pivotpl_data/S12017MeanClusterEdustd.csv")
+
+# Merge the mean per dept into the main data set
+S12017RespData <- merge(x = S12017RespData, y = S12017MeanClusterEdustd, by = c("cluster_tag", "question_group_id"), all.x = TRUE)
+
+#####################
+
+
+# Mean of responses by school by Teacher
+####################
+
+#aggreagte by teacher ID
+S12017MeanSchoolTeacher <- aggregate(S12017RespData$answer_num, by = list(S12017RespData$acara_id, S12017RespData$school_name, S12017RespData$teacher_id), mean)
+
+# Set column names
+colnames(S12017MeanSchoolTeacher) <- c("acara_id", "School Name", "teacher_id", "answer_num_teacher_mean")
+
+## Write this to a file
+write.csv(S12017MeanSchoolTeacher, file = "C:/users/rakkhi/documents/pivotpl_data/S12017MeanSchoolTeacher.csv")
+
+################
+
+
+# Mean of responses by school by Teacher with Dept Mean
+####################
+
+#aggreagte by teacher ID
+S12017MeanSchoolTeacherDeptComp <- aggregate(S12017RespData$answer_num, by = list(S12017RespData$acara_id, S12017RespData$school_name, S12017RespData$cluster_tag, S12017RespData$teacher_id, S12017RespData$answer_num_cluster_mean), mean)
+
+# Set column names
+colnames(S12017MeanSchoolTeacherDeptComp) <- c("acara_id", "School Name", "Dept_Name", "teacher_id", "Dept mean", "answer_num_teacher_mean")
+
+## Write this to a file
+write.csv(S12017MeanSchoolTeacherDeptComp, file = "C:/users/rakkhi/documents/pivotpl_data/S12017MeanSchoolTeacherDeptComp.csv")
+
+################
+
+
+# Mean of responses by school by Teacher with Dept Mean by Education Standard
+####################
+
+#aggreagte by teacher ID
+S12017MeanSchoolTeacherDeptCompEdustd <- 
+  aggregate(S12017RespData$answer_num, by = list(S12017RespData$acara_id, S12017RespData$school_name, S12017RespData$cluster_tag, S12017RespData$teacher_id, S12017RespData$question_group_id, S12017RespData$answer_num_cluster_mean_edustd), mean)
+
+# Set column names
+colnames(S12017MeanSchoolTeacherDeptCompEdustd) <- c("acara_id", "School Name", "Dept_Name", "teacher_id", "Edu Std Group", "Edu Std Group Dept mean", "answer_num_teacher_mean")
+
+## Write this to a file
+write.csv(S12017MeanSchoolTeacherDeptCompEdustd, file = "C:/users/rakkhi/documents/pivotpl_data/S12017MeanSchoolTeacherDeptCompEdustd.csv")
+
+################
+
+
+#2018 data
+
 # Mean of responses by school
 
 aggregate(Q12018RespData$answer_num, by = list(Q12018RespData$school_name), mean)
--- Dataload.r
+++ Dataload.r
@@ -1,3 +1,49 @@
+#2017 data
+# Read in the data files
+S12017RespData <- read.csv("C:/users/rakkhi/documents/pivotpl_data/acara_exports/all_responses_2017-01-01_2017-07-23.csv")
+
+#show rowcount
+nrow(S12017RespData)
+
+#Remove test data for Fake high school and Australia High School 
+S12017RespData <- S12017RespData[!(S12017RespData$acara_id==110),]
+S12017RespData <- S12017RespData[complete.cases(S12017RespData$acara_id),]
+
+#show rowcount
+nrow(S12017RespData)
+
+#show column headers test
+colnames(S12017RespData)
+
+# Remove the meta columns:
+S12017RespData <- S12017RespData[ -c (21:42)]
+
+#show column headers test
+colnames(S12017RespData)
+
+# Create a new column to store the answer data as numeric
+S12017RespData$answer_num<-NA
+
+# Convert responses to numeric
+S12017RespData$answer_num <-
+  ifelse(S12017RespData$answer == 'Strongly disagree', 1,
+         ifelse(S12017RespData$answer == 'Disagree', 2,
+                ifelse(S12017RespData$answer == 'Neither agree nor disagree', 3,
+                       ifelse(S12017RespData$answer == 'Agree', 4,
+                              ifelse(S12017RespData$answer == 'Strongly agree', 5, 0)
+                       )
+                )
+         )
+  )
+
+#show column headers test
+colnames(S12017RespData)
+
+#show response_num data test
+S12017RespData$answer_num
+
+
+#2018 data
 # Read in the data files
 Q12018RespData <- read.csv("C:/users/rakkhi/documents/pivotpl_data/all_responses_2018-01-01_2018-04-12.csv")
 
@@ -24,3 +70,6 @@ colnames(Q12018RespData)
 
 #show response_num data test
 Q12018RespData$answer_num
+
+#show rowcount
+nrow(Q12018RespData)
